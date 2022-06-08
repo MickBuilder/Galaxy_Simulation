d68d609fc4c6b45e663bdacaa9d71c377b257f5b
Mick Cool <bmikdaam@gmail.com>
2022-06-08 07:15:25 +0200

    adding the change of galaxy file

--- src/galaxy.c
+++ src/galaxy.c
@@ -36,24 +36,60 @@ void init_galaxy(FILE *file, Galaxy *galaxy) {
 }
 
 void update_galaxy(Galaxy *galaxy, Quadtree *quadtree) {
-    /*for (int i = 0; i < galaxy->nb_stars; i++) {
+    for (int i = 0; i < galaxy->nb_stars; i++) {
         Star *star = galaxy->stars[i];
-        init_acceleration(&star);
-        for (int j = 0; j < galaxy->nb_stars; j++) {
-            if (i != j) {
-                Star *other_star = galaxy->stars[j];
-                update_star_acceleration(&star, other_star->position, other_star->mass);
+        if (star != NULL) {
+            init_acceleration(&star);
+            for (int j = 0; j < galaxy->nb_stars; j++) {
+                if (i != j) {
+                    Star *other_star = galaxy->stars[j];
+
+                    if (other_star != NULL) {
+                        if(distance(star->position, other_star->position) < MIN_DISTANCE_BETWEEN_STARS
+                            && star->mass > other_star->mass) {
+                            printf("too close. Eat him!\n");
+                            merge_star(&star, other_star);
+                        } else {
+                            update_star_acceleration(&star, other_star->position, other_star->mass);
+                        }
+                    } else {
+                        printf("other star is null\n");
+                        continue;
+                    }
+                }
             }
         }
     }
 
     for (int i = 0; i < galaxy->nb_stars; i++) {
         Star *star = galaxy->stars[i];
-        update_star_velocity(&star);
-        update_star_position(&star);
+        if (star != NULL) {
+            update_star_velocity(&star);
+            update_star_position(&star);
+        } else {
+            printf("star is null\n");
+            continue;
+        }
+        /*update_star_velocity(&star);
+        update_star_position(&star);*/
+    }
+
+    /*for (int i = 0; i < galaxy->nb_stars; i++) {
+        Star *star = galaxy->stars[i];
+        for (int j = 0; j < galaxy->nb_stars; j++) {
+            if (i != j) {
+                Star *other_star = galaxy->stars[j];
+                if (star != NULL && other_star != NULL
+                    && distance(star->position, other_star->position) < MIN_DISTANCE_BETWEEN_STARS
+                    && star->mass > other_star->mass) {
+                    printf("to close. Eat him!\n");
+                    merge_star(&star, other_star);
+                }
+            }
+        }
     }*/
 
-    Region *region = create_region(-galaxy->widthOfRegion/2, -galaxy->widthOfRegion/2, galaxy->widthOfRegion, galaxy->widthOfRegion);
+    /*Region *region = create_region(-galaxy->widthOfRegion/2, -galaxy->widthOfRegion/2, galaxy->widthOfRegion, galaxy->widthOfRegion);
     quadtree = create_quadtree(*region);
 
     for (int i = 0; i < galaxy->nb_stars; i++) {
@@ -74,7 +110,7 @@ void update_galaxy(Galaxy *galaxy, Quadtree *quadtree) {
         update_star_position(&star);
     }
 
-    free_quadtree(quadtree);
+    free_quadtree(quadtree);*/
 }
 
 void free_galaxy(Galaxy *galaxy) {
